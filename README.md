1c895ea8df120237d651bd1b48384a8893706793
speeditor <speeditwikia@gmail.com>
2018-07-24 19:17:32 +0100

    v0.8.0: package as Chrome extension

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
-# oasis-discussions
-OasisDiscussions wrapper and events library for FANDOM Discussions.
+# demeter
+Chrome extension that implements an Oasis design wrapper and userscript library for FANDOM Discussions.
 
 ## Documentation
-The script creates a discussions object.
-* `discussions.events` - Discussions DOM events creation
+The script creates a `window.demeter` object.
+
+Modules:
+* `demeter.events` - Discussions DOM events creation
   * `"discussions.boot"` - when Discussions is booting
   * `"discussions.init"` - when Discussions has initialized
   * `"discussions.navi"` - Discussions completing user navigation
   * `"discussions.mobi"` - Discussions entering the mobile responsive breakpoint
   * `"discussions.wide"` - Discussions entering the desktop responsive breakpoint
+* `demeter.config` - Discussions configuration variables
+  * `demeter.config.get` - get configuration by dot-delimited path string
+  * `demeter.config.set` - set config value by dot-delimited path string
 
 ## TO-DO
-* UI modifications for consistency with Oasis
-* Rail activity module
 * API wrapper
---
+++ app.js
@@ -0,0 +1,286 @@
+// Application scripting
+document.body.addEventListener('animationstart', function(e) {
+    // Scope restriction
+    if (e.animationName !== 'discussions__init') {
+        return;
+    }
+    window.demeter = {};
+    // Discussions events module.
+    demeter.events = {
+        t: 'animationstart',
+        handler: function(e) {
+            var a = e.originalEvent.animationName.split('__');
+            if (
+                a[0] !== 'discussions' ||
+                !window.oasisDiscussionsInitialized
+            ) {
+                return;
+            }
+            demeter.hook('discussions.' + a[1]).fire({ 'init': true });
+        }
+    };
+    // Userscripting hook module.
+    demeter.hook = (function() {
+        var lists = {};
+        return function(name) {
+            var list = lists[name] || (lists[name] = $.Callbacks('memory'));
+            return {
+                /**
+                 * @function    demeter.hook.add
+                 * @param       {function} fn Handler.
+                 */
+                add: list.add,
+                /**
+                 * @function    demeter.hook.remove
+                 * @param       {function} fn Handler.
+                 */
+                remove: list.remove,
+                /**
+                 * @function    demeter.hook.fire
+                 * @param       {*} arg Argument.
+                 */
+                fire: function() {
+                    return list.fireWith(null, [].slice.call(arguments));
+                }
+            };
+        };
+    }());
+    // Configuration module.
+    demeter.config = {
+        init: function() {
+            $('script[type="fastboot/shoebox"]').each(function() {
+                var sid = this.id.match(/shoebox-([\s\S]+)$/)[1];
+                demeter.config._values[sid] = JSON.parse(this.textContent);
+            });
+        },
+        // Configuration getter.
+        get: function(s) {
+            return s.split('.').reduce(function(o, a) {
+                return o[a];
+            }, demeter.config._values);
+        },
+        // Configuration setter.
+        set: function(s, v) {
+            if (typeof s === 'string') {
+                return demeter.config.set(
+                    demeter.config._values,
+                    s.split('.'),
+                    v
+                );
+            } else if (s.length === 1 && v) {
+                return (demeter.config._values[s[0]] = v);
+            } else if (s.length === 0) {
+                return demeter.config._values;
+            } else {
+                return demeter.config.set(
+                    demeter.config._values[s[0]],
+                    s.slice(1),
+                    v
+                );
+            }
+        },
+        // Configuration cache.
+        _values: {}
+    };
+    // Oasis module.
+    demeter.oasis = {
+        // Oasis module initialiser.
+        init: function(d) {
+            // Function variables.
+            var w = demeter.config.get('applicationData.wikiVariables');
+            // Oasis styling.
+            $('<link>', {
+                'rel': 'stylesheet',
+                'href': [
+                    'https://cors-anywhere.herokuapp.com',
+                    'https://slot1-images.wikia.nocookie.net/__am',
+                    w.cacheBuster,
+                    'sasses',
+                    encodeURIComponent($.param(w.theme)),
+                    'skins/oasis/css/core/breakpoints-background.scss'
+                ].join('/'),
+                'crossorigin': 'anonymous'
+            }).appendTo(document.body);
+            // Styling generator.
+            $('<style>', {
+                'id': 'demeter_css',
+                'text': (function(t) {
+                    return ':root { ' + Object.keys(t).map(function(p) {
+                        return '--' + p + ': ' + t[p] + ';';
+                    }).join(' ') + ' }';
+                }(w.theme))
+            }).appendTo(document.body);
+            // Data fetching.
+            demeter.oasis.data(d);
+        },
+        // Oasis data handler.
+        data: function(d) {
+            // Wiki data.
+            var t = new Date().getTime(),
+                cb = Number(localStorage.demeterAge) || +t-21600000,
+                s = (typeof localStorage.demeterSettings === 'undefined');
+            // Theme setup.
+            if (s || (t >= +cb+300000)) {
+                $.get('/wiki/Special:BlankPage').done(function(w) {
+                    demeter.oasis.call(w, d);
+                });
+            } else {
+                demeter.oasis.themeSettings = JSON.parse(localStorage.demeterSettings);
+                demeter.oasis.theme(d);
+            }
+        },
+        // Oasis theme settings extraction.
+        call: function(w, d) {
+            // Body class retrieval.
+            var wc = w
+                .match(/<body class="([^"]*)"/)[1]
+                .split(' ').filter(function(u) {
+                    return !!u.length;
+                });
+            // Assign design settings.
+            $.each(demeter.oasis.classMap, function(s, p) {
+                var c = (wc.indexOf(p.className) > -1);
+                demeter.oasis.themeSettings[s] = p.reverse ? !c : c;
+            });
+            // Data caching.
+            localStorage.demeterSettings = JSON.stringify(demeter.oasis.themeSettings);
+            localStorage.demeterAge = new Date().getTime();
+            // Theming utility.
+            demeter.oasis.theme(d);
+        },
+        // Oasis theme configuration.
+        theme: function(d) {
+            if (d.init) {
+                demeter.oasis.classList = ['skin-oasis'];
+            }
+            // Class iterator.
+            $.each(demeter.oasis.themeSettings, function(s, p) {
+                if (demeter.oasis.classMap[s].reverse === p) { return; }
+                var cls = demeter.oasis.classMap[s].className;
+                demeter.oasis.classList.push(cls);
+            });
+            // Script rendering.
+            if (!d.init) {
+                demeter.oasis.render();
+            }
+            demeter.oasis.util.addClass(d);
+        },
+        // Oasis renderer.
+        render: function() {
+            // Script activation.
+            demeter.hook('discussions.init').add(demeter.oasis.handler.on);
+            // Event delegation.
+            demeter.hook('discussions.navi').add(demeter.oasis.handler.on);
+            demeter.hook('discussions.mobi').add(demeter.oasis.handler.off);
+            demeter.hook('discussions.wide').add(demeter.oasis.handler.on);
+            demeter.hook('discussions.init').add(demeter.oasis.handler.on);
+        },
+        // Oasis event handlers.
+        handler: {
+            // Oasis styling activator.
+            on: function() {
+                var t = new Date().getTime(),
+                    cb = Number(localStorage.demeterAge) || +t-21600000,
+                    s = (typeof localStorage.demeterSettings === 'undefined');
+                // Remove existing classes.
+                demeter.oasis.util.rmvClass();
+                // Theme setup.
+                if (s || (t >= +cb+21600000)) {
+                    demeter.oasis.data({ 'init': true });
+                } else {
+                    demeter.oasis.util.addClass();
+                }
+            },
+            // Oasis styling deactivator.
+            off: function() {
+                demeter.oasis.util.rmvClass();
+            }
+        },
+        // Oasis module utilities.
+        util: {
+            // Oasis class addition.
+            addClass: function() {
+                demeter.oasis.classList.forEach(function(cls) {
+                    $(document.body).addClass(cls);
+                });
+            },
+            // Oasis class removal.
+            rmvClass: function() {
+                demeter.oasis.classList.forEach(function(cls) {
+                    $(document.body).removeClass(cls);
+                });
+            }
+        },
+        // Class list cache.
+        classList: ['skin-oasis'],
+        // Class map.
+        classMap: {
+            'background-fix': {
+                reverse: false,
+                className: 'background-fixed'
+            },
+            'background-split': {
+                reverse: true,
+                className: 'background-not-split'
+            },
+            'background-tile': {
+                reverse: true,
+                className: 'background-not-tiled'
+            }
+        },
+        // Settings cache.
+        themeSettings: {}
+    };
+    /**
+     * Module registry
+     * @member {Array} od.modules
+     */
+    demeter.modules = Object.keys(demeter).filter(function(m) {
+        return (
+            typeof demeter[m] === 'object' &&        // module check
+            typeof demeter[m].init === 'function'    // init fn check
+        );
+    });
+    /**
+     * Script initializer
+     * @method od.init
+     */
+    demeter.init = (function() {
+        // Global status variable
+        window.oasisDiscussionsInitialized = true;
+        // Initial hook
+        $(document.body).trigger('discussions.init');
+        // Event dispatcher
+        $(document.body).on(demeter.events.t, demeter.events.handler);
+        // Module initializers
+        demeter.modules.forEach(function(m) {
+            demeter[m].init({ 'init': false });
+        });
+    }());
+});
+// Application styling.
+(function(app) {
+    // Stylesheet attributes.
+    Object.keys(app.m).forEach(function(a) {
+        app.s.setAttribute(a, app.m[a]);
+    });
+    // Stylesheet insertion.
+    document.body.appendChild(app.s);
+}({
+    // Stylesheet element.
+    s: document.createElement('link'),
+    // Stylesheet attributes.
+    m: {
+        'rel': 'stylesheet',
+        'href': (function(o) {
+            return '/load.php?' + Object.keys(o).map(function(k) {
+                return encodeURIComponent(k) + '=' + encodeURIComponent(o[k]);
+            }).join('&');
+        }({
+            mode: 'articles',
+            articles: 'u:speedit:MediaWiki:OasisDiscussions.css',
+            only: 'styles',
+            debug: '1'
+        }))
+    }
+}));
